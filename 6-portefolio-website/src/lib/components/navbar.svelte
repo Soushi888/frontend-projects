9606260e6f42dd0fff10a0ee69bdd80fda77a1d8
Soushi888 <sacha.pignot@protonmail.com>
2024-11-04 20:15:14 -0500

    ✨ feat(project 6): start the fundations of the protefolio website

---
+++ 6-portefolio-website/src/lib/components/navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/projects">Projects</a>
+	<a href="/skills">Skills</a>
+	<a href="/contact">Contact me</a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
+		height: 10vh;
+		background-color: rgb(8, 8, 8);
+	}
+
+	a {
+		text-decoration: none;
+		color: var(--primary);
+
+		&:hover {
+			border-bottom: 1px solid white;
+		}
+	}
+</style>
