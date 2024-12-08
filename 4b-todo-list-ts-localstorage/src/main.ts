25668d185d666ac404016f6dc0289dfb60baf2a2
soushi888 <sacha.pignot@protonmail.com>
2024-12-08 18:12:58 -0500

    Remove TypeScript project directories 4a-counter-ts and 4b-todo-list-ts-localstorage
    - Deleted all files from counter and todo list TypeScript projects
    - Includes removal of source files, configuration, and package management files

--- 4b-todo-list-ts-localstorage/src/main.ts
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-import { TodoList } from "./todo-list";
-
-const todoList = new TodoList();
-todoList.loadFromLocalStorage();
-
-const saveIconEl: HTMLDivElement = document.querySelector("#save-icon")!;
-saveIconEl.addEventListener("click", () => todoList.saveToLocalStorage());
